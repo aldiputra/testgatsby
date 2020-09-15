9e28f4de8a0971bb50b0716a6c34f899243e3585
Aldi Cita Putra <aldi.putra@photoninfotech.net>
2020-09-16 01:03:16 +0700

    Change value max-age

--- gatsby-config.js
+++ gatsby-config.js
@@ -41,25 +41,33 @@ module.exports = {
       resolve: `gatsby-plugin-netlify`,
       options: {
         headers: {
-          "/**/*.html": [
-            "cache-control: public",
-            "cache-control: max-age=0",
-            "cache-control: must-revalidate",
-          ],
           "/page-data/*.json": [
             "cache-control: public",
             "cache-control: max-age=1000",
             "cache-control: must-revalidate",
           ],
-          "/app-data.json": [
+          "/**.js": [
             "cache-control: public",
-            "cache-control: max-age=1000",
-            "cache-control: must-revalidate",
+            "cache-control: max-age=2592000",
+          ],
+          "/**.css": [
+            "cache-control: public",
+            "cache-control: max-age=2592000",
           ],
           "/static/*": [
             "cache-control: public",
             "cache-control: max-age=31536000",
             "cache-control: immutable",
+          ],
+          "/": [
+            "cache-control: private",
+            "cache-control: no-cache",
+            "cache-control: max-age=3600",
+          ],
+          "/blog/**": [
+            "cache-control: private",
+            "cache-control: no-cache",
+            "cache-control: max-age=3600",
           ],
         },
       },
